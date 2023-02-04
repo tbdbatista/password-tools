bbafc79741933708c9a0bf3b3cab0215b557e425
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-02-04 17:57:52 -0300

    Refactoring - Extract uiView components from PasswordViewController to PasswordView - Thiago Batista

---
+++ PasswordTools/PasswordView.swift
@@ -0,0 +1,69 @@
+//
+//  PasswordView.swift
+//  PasswordTools
+//
+//  Created by Beltrami Dias Batista, Thiago on 04/02/23.
+//
+
+import UIKit
+
+class PasswordView: UIView {
+
+    lazy var stackView = UIStackView()
+    lazy var passwordView = PasswordMainView(placeHolderText: "New password")
+    lazy var criteriaView = CriteriaView()
+    lazy var secondaryPasswordView = PasswordMainView(placeHolderText: "Re-enter new password")
+    lazy var resetButton = UIButton()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSelfView()
+        setupComponentsViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup Components
+    private func setupSelfView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .white
+        self.addSubview(stackView)
+        self.stackView.addArrangedSubview(passwordView)
+        self.stackView.addArrangedSubview(criteriaView)
+        self.stackView.addArrangedSubview(secondaryPasswordView)
+        self.stackView.addArrangedSubview(resetButton)
+        self.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+    }
+
+    private func setupComponentsViews() {
+        setupStackView()
+        setupCriteriaView()
+        setupResetButton()
+    }
+
+    private func setupStackView() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: self.leadingAnchor, multiplier: 1),
+            self.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+
+    private func setupCriteriaView() {
+        criteriaView.layer.cornerRadius = 4
+        criteriaView.backgroundColor = .tertiarySystemFill
+        criteriaView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+
+    private func setupResetButton() {
+        resetButton.setTitle("Reset password", for: [])
+        resetButton.configuration = .filled()
+//        resetButton.addTarget(self, action: #selector(didTapConfirmPassword), for: .touchUpInside)
+    }
+}
--- PasswordTools/PasswordViewController.swift
+++ PasswordTools/PasswordViewController.swift
@@ -9,61 +9,22 @@ import UIKit
 
 class PasswordViewController: UIViewController {
 
-    lazy var stackView = UIStackView()
-    lazy var passwordView = PasswordMainView(placeHolderText: "New password")
-    lazy var criteriaView = CriteriaView()
-    lazy var secondaryPasswordView = PasswordMainView(placeHolderText: "Re-enter new password")
-    lazy var resetButton = UIButton()
+    let passwordView = PasswordView(frame: .zero)
 
     // MARK: - ViewController Lifecycle
+    override func loadView() {
+        view = passwordView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSelfView()
-        setupComponentViews()
-        passwordView.textField.delegate = self
-        secondaryPasswordView.textField.delegate = self
+        passwordView.passwordView.textField.delegate = self
+        passwordView.secondaryPasswordView.textField.delegate = self
     }
 
-    // MARK: - Setup Components
-    private func setupSelfView() {
-        self.view.translatesAutoresizingMaskIntoConstraints = false
-        self.view.backgroundColor = .white
-        self.view.addSubview(stackView)
-        self.stackView.addArrangedSubview(passwordView)
-        self.stackView.addArrangedSubview(criteriaView)
-        self.stackView.addArrangedSubview(secondaryPasswordView)
-        self.stackView.addArrangedSubview(resetButton)
-        self.view.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
-    }
-
-    private func setupComponentViews() {
-        setupStackView()
-        setupCriteriaView()
-        setupResetButton()
-    }
-
-    private func setupStackView() {
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 20
-
-        NSLayoutConstraint.activate([
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
-        ])
-    }
-
-    private func setupCriteriaView() {
-        criteriaView.layer.cornerRadius = 4
-        criteriaView.backgroundColor = .tertiarySystemFill
-        criteriaView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-    }
-
-    private func setupResetButton() {
-        resetButton.setTitle("Reset password", for: [])
-        resetButton.configuration = .filled()
-        resetButton.addTarget(self, action: #selector(didTapConfirmPassword), for: .touchUpInside)
+    // MARK: - Setup Actions
+    private func setupActions() {
+        passwordView.resetButton.addTarget(self, action: #selector(didTapConfirmPassword), for: .touchUpInside)
     }
 
     // MARK: - Actions
@@ -75,10 +36,10 @@ class PasswordViewController: UIViewController {
 
     // MARK: - Methods
     private func confirmReenteredPassword() {
-        guard let firstPassword = passwordView.textField.text,
-              passwordView.textField.text != "",
-              let secondPassword = secondaryPasswordView.textField.text,
-              secondaryPasswordView.textField.text != "" else {
+        guard let firstPassword = passwordView.passwordView.textField.text,
+              passwordView.passwordView.textField.text != "",
+              let secondPassword = passwordView.secondaryPasswordView.textField.text,
+              passwordView.secondaryPasswordView.textField.text != "" else {
                   print("senhas não podem ser nulas")
                   return }
         if  firstPassword == secondPassword {
@@ -89,8 +50,8 @@ class PasswordViewController: UIViewController {
     }
 
     private func textFieldResignFirstResponder() {
-        passwordView.textField.resignFirstResponder()
-        secondaryPasswordView.textField.resignFirstResponder()
+        passwordView.passwordView.textField.resignFirstResponder()
+        passwordView.secondaryPasswordView.textField.resignFirstResponder()
     }
 }
 
