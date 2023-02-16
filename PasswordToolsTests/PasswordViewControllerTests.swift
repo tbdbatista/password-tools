99c805c32893df486e11d868291f86af57563356
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-02-16 13:20:41 -0300

    SwifLint - Add string size config - Thiago Batista

---
+++ PasswordToolsTests/PasswordViewControllerTests.swift
@@ -0,0 +1,8 @@
+//
+//  PasswordViewControllerTests.swift
+//  PasswordToolsTests
+//
+//  Created by Beltrami Dias Batista, Thiago on 15/02/23.
+//
+
+import Foundation
---
+++ PasswordToolsTests/PasswordViewTests.swift
@@ -0,0 +1,8 @@
+//
+//  PasswordViewTests.swift
+//  PasswordToolsTests
+//
+//  Created by Beltrami Dias Batista, Thiago on 15/02/23.
+//
+
+import Foundation
